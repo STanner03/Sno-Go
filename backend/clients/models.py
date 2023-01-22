287a9331334646cb8794f6c124000fee3f318fff
Shane <shanetanner03@gmail.com>
2023-01-21 20:53:32 -0600

    Defined models.py for providers

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from authentication.models import User
+
 
 # Create your models here.
 
 class Client(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.IntegerField()
--- backend/providers/models.py
+++ backend/providers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from authentication.models import User
+
 
 # Create your models here.
+
+class Provider(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=255)
+    email = models.CharField(max_length=75)
+    equipment = models.ForeignKey(, on_delete=models.CASCADE)
+    services = models.CharField(max_length=255)
+    business_description = models.CharField(max_length=255)
+    payment_type = models.CharField(max_length=255)
