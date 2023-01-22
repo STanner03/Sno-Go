164cc7d3c2c779c653c4f41f3a59e210d37eeb30
Shane <shanetanner03@gmail.com>
2023-01-21 21:12:56 -0600

    Defined models.py for equipment + made imports for foreign keys where necessary in current apps.

--- backend/providers/models.py
+++ backend/providers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from authentication.models import User
+from equipment.models import Equipment
 
 
 # Create your models here.
@@ -11,7 +12,7 @@ class Provider(models.Model):
     phone = models.IntegerField()
     address = models.CharField(max_length=255)
     email = models.CharField(max_length=75)
-    # equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     services = models.CharField(max_length=255)
     business_description = models.CharField(max_length=255)
     payment_type = models.CharField(max_length=255)
