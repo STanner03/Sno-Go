6d018c0a623235c834c2c5aaaf56ca6a12858cea
Shane <shanetanner03@gmail.com>
2023-01-21 19:14:52 -0600

    Created clients APP + urls & serializers files

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from clients.models import Client
-from providers.models import Provider
+# from clients.models import Client
+# from providers.models import Provider
 
 # Create your models here.
 
 class Job(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    # client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    # provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
     area_type = models.CharField(max_length=255)
     photo_required = models.BooleanField()
