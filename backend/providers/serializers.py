5bfa36afa2ea4a41f1dabf1990e138a57928c966
Shane <shanetanner03@gmail.com>
2023-01-23 15:33:26 -0600

    Defined serializers.py for providers

---
+++ backend/providers/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Provider
+
+
+class ProviderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = ['id', 'user_id', 'first_name', 'last_name',
+                  'phone', 'address', 'email', 'equipment_id', 'service_id', 'business_description', 'payment_type']
+        depth = 1
