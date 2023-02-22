331bab0bf5b60f185a6bffd8607c53bcad163441
Shane Tanner <shanetanner0322@gmail.com>
2023-02-22 16:50:57 -0600

    Updated URLs and Views for Equipment to allow endpoints to function correctly.
    Also imported user_passes_test and REDIRECT_FIELD_NAME to enable only staff to add or delete equipment.

--- backend/equipment/urls.py
+++ backend/equipment/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from equipment import views
 
 urlpatterns = [
-    path('', views.user_equipment),
+    path('add/', views.add_equipment),
     path('all/', views.get_all_equipment),
-    path('all/', views.get_providers_equipment),
+    path('provider/', views.get_providers_equipment),
     path('<int:pk>/update/', views.update_equipment),
 ]
--- backend/equipment/views.py
+++ backend/equipment/views.py
@@ -4,11 +4,26 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import user_passes_test
 from .models import Equipment
 from .serializers import EquipmentSerializer
 
 # Create your views here.
 
+def staff_member_required(view_func=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='admin:login'):
+    """
+    Decorator for views that checks that the user is logged in and is a staff
+    member, redirecting to the login page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.is_staff,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if view_func:
+        return actual_decorator(view_func)
+    return actual_decorator
 
 @api_view(['Get'])
 @permission_classes([AllowAny])
@@ -21,23 +36,20 @@ def get_all_equipment(request):
 @api_view(['Get'])
 @permission_classes([AllowAny])
 def get_providers_equipment(request, ppk):
-    equipment = Equipment.objects.filter(provider_id=ppk)
+    equipment = Equipment.objects.filter(provider=ppk)
     serializer = EquipmentSerializer(equipment, many=True)
     return Response(serializer.data)
 
 
-@api_view(['POST', 'GET'])
-@permission_classes([IsAuthenticated])
-def user_equipment(request, ppk):
+@api_view(['POST'])
+@staff_member_required()
+def add_equipment(request):
+    print("ADD EQUIPMENT", request)
     if request.method == 'POST':
         serializer = EquipmentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(user_id=request.user.id, provider_id=ppk)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    elif request.method == 'GET':
-        equipment = Equipment.objects.filter(user_id=request.user.id)
-        serializer = EquipmentSerializer(equipment, many=True)
-        return Response(serializer.data)
 
 
 @api_view(['PUT', 'DELETE'])
